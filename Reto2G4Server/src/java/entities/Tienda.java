42786a35ce54e5c49f2cbae87b554ddf2a2bd0cc
davidnardiz <davidnardiztartanga@gmail.com>
2023-12-15 10:19:30 +0100

    Entity tienda y enum tipoPago

---
+++ Reto2G4Server/src/java/entities/Tienda.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entitys;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author David
+ */
+@Entity
+@Table(name = "SECTOR", schema = "emex51db")
+public class Tienda implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idTienda;
+    private String nombre;
+    private String descripcion;
+    @Enumerated(EnumType.STRING)
+    private TipoPago tipoPago;
+    private double espacio;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fechaCreacion;
+    private int idCliente;
+    @OneToOne(mappedBy = "tienda", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Cliente cliente;
+    
+
+    public Integer getIdTienda() {
+        return idTienda;
+    }
+
+    public void setIdTienda(Integer idTienda) {
+        this.idTienda = idTienda;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public TipoPago getTipoPago() {
+        return tipoPago;
+    }
+
+    public void setTipoPago(TipoPago tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+
+    public double getEspacio() {
+        return espacio;
+    }
+
+    public void setEspacio(double espacio) {
+        this.espacio = espacio;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idTienda != null ? idTienda.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Tienda)) {
+            return false;
+        }
+        Tienda other = (Tienda) object;
+        if ((this.idTienda == null && other.idTienda != null) || (this.idTienda != null && !this.idTienda.equals(other.idTienda))) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+}
