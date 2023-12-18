0ba589667479e214f09ef35611ffe1b672ebc3a2
davidnardiz <davidnardiztartanga@gmail.com>
2023-12-18 10:39:44 +0100

    Entidades terminadas

--- Reto2G4Server/src/java/entities/Producto.java
+++ Reto2G4Server/src/java/entities/Producto.java
@@ -6,9 +6,11 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Objects;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
